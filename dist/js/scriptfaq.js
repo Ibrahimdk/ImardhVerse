c8087b8fea8eb130792e8ac67074086316a64b27
Ibrahimdk <ibrahimmahardhikaid@gmail.com>
2023-08-02 12:49:08 +0700

    First Time Deoploy

---
+++ dist/js/scriptfaq.js
@@ -0,0 +1,11 @@
+const faqList = document.querySelectorAll(".faq");
+
+faqList.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding : '16px',
+    },
+    extend: {
+      fontFamily: {
+        goldman : ['Goldman','cursive'],
+        dokdo: ['Dokdo', 'cursive'],
+        poppins: ['Poppins', 'sans-serif'],
+        
+      },
+      colors: {
+        blacky: "#0A0A0A",
+        neon: "#C8FF53",
+        grey: "#CECECE",
+        yellows : "#FFE486",
+        darkgrey : "#272727",
+        softgrey : "#373737",
+      },
+      screens : {
+        '2xl' : '1320px',
+      },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        } ,
+        slidereverse: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        } ,
+    },
+    animation: {
+      slide : 'slide 5s infinite linear ',
+      slidereverse : 'slidereverse 5s infinite linear ',
+    },
+  },
+  plugins: [],
+}
+}
